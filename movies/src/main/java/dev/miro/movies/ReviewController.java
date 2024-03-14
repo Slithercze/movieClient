e3ee5999f9b702f1908ece48512814d969c50966
Slithercze <Slithercze@gmail.com>
2024-03-14 23:29:33 +0100

    fullstack app

---
+++ movies/src/main/java/dev/miro/movies/ReviewController.java
@@ -0,0 +1,21 @@
+package dev.miro.movies;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload) {
+        return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+}
